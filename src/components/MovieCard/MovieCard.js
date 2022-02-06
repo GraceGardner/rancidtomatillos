41de85bdeedd1b67bacf157679a4ce2260ddfa4b
Ali Roemhildt <aliroemhildt@gmail.com>
2022-02-05 21:51:50 -0700

    Complete styling for movie cards

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -3,11 +3,14 @@ import './MovieCard.scss';
 const MovieCard = ({image, title, id, selectMovie}) => {
   return (
     <div className='card-container'>
-      <button
+      <div
+        className='card'
         id={id}
-        onClick={() => selectMovie(id)}>
+        onClick={() => selectMovie(id)}
+        tabIndex='0'
+        role='button'>
         <img className='card-image' src={image} alt={title + ' cover'}/>
-      </button>
+      </div>
       <h3 className='card-text'>{title}</h3>
     </div>
   )
