2a0f0b871563c2b340b6673dead21606a6b1989e
Grace Gardner <graceirisgardner@gmail.com>
2022-02-06 14:48:44 -0700

    add styling to containers

--- src/components/SingleMovie/SingleMovie.js
+++ src/components/SingleMovie/SingleMovie.js
@@ -4,15 +4,30 @@ import './SingleMovie.scss';
 const SingleMovie = ({movie}) => {
   return (
     <div className='single-movie-container'>
-      <h2>{movie.title}</h2>
-      <h3>{movie.tagline}</h3>
-      <img src={movie.backdrop_path} alt={movie.title + ' cover'}/>
+      <header className='single-movie-header'>
+      <div className='title-container'>
+        <h2 className='movie-title'>{movie.title}</h2>
+        <h3>{movie.tagline}</h3>
+      </div>
+      <div className='header-details'>
+        <div className='runtime-container'>
+          <p className='bold-text'>Runtime:</p>
+          <p>{movie.runtime} minutes</p>
+        </div>
+        <div className='rating-container'>
+          <p className='bold-text'>Rating:</p>
+          <p>{movie.average_rating.toFixed(1)}</p>
+        </div>
+      </div>
+      </header>
+      <div className='image-container'>
+        <img className='side-img' src={movie.poster_path} alt={movie.title + ' cover'}/>
+        <img className='backdrop-img' src={movie.backdrop_path} alt={movie.title + ' backdrop image'}/>
+      </div>
       <div className='details-row'>
         <div className='movie-datails'>
           <p>Genre: {movie.genres.join(', ')}</p>
-          <p>Rating: {movie.average_rating.toFixed(1)}</p>
           <p>Release Date: {movie.release_date}</p>
-          <p>Runtime: {movie.runtime} minutes</p>
           <p>Budget: ${movie.budget}</p>
           <p>Revenue: ${movie.revenue}</p>
         </div>
