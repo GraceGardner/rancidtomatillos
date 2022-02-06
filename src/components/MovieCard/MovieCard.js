6a0c1566c0141997e7614b6cdfbed71d51d8f513
Ali Roemhildt <aliroemhildt@gmail.com>
2022-02-05 19:05:23 -0700

    Edit card styling WIP

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -2,13 +2,14 @@ import './MovieCard.scss';
 
 const MovieCard = ({image, title, id, selectMovie}) => {
   return (
-    <button
-      className='card'
-      id={id}
-      onClick={() => selectMovie(id)}>
-      <img className='card-image' src={image} alt={title + ' cover'}/>
-      <h3>{title}</h3>
-    </button>
+    <div className='card-container'>
+      <button
+        id={id}
+        onClick={() => selectMovie(id)}>
+        <img className='card-image' src={image} alt={title + ' cover'}/>
+      </button>
+      <h3 className='card-text'>{title}</h3>
+    </div>
   )
 }
 
